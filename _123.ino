a22d603d13ae1b28037a8fbc26f1c1b1044dd5f2
vinod-sai <vinod.sai@riktamtech.com>
2017-08-16 16:22:22 +0530

    hi testing

---
+++ _123.ino
@@ -0,0 +1,21 @@
+#include <BlynkSimpleEsp8266.h>
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);  // initialize onboard LED as output
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  pinMode(D2, OUTPUT);
+  
+}
+int v;
+ 
+void loop() {
+ // Serial.println("HI");
+  digitalWrite(D2,HIGH);
+  v= digitalRead(D1);
+  Serial.println(v);
+  digitalWrite(0, HIGH);  // turn on LED with voltage HIGH
+  //delay(1000);  // wait one second
+ // digitalWrite(D2,LOW);
+  digitalWrite(2, LOW);   // turn off LED with voltage LOW
+  //delay(1000);                      // wait one second
+}
